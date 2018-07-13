a2c0a1505b0a8ab9494074c5fe589d003067c5bf
iyaja <ajayuppili@gmail.com>
2018-07-13 23:04:58 +0530

    Started code for "classes".

--- src/Spices.kt
+++ src/Spices.kt
@@ -1,30 +1,42 @@
-class Spice(val name: String, val spiciness: String = "mild") {
+import java.awt.Label
 
-    val heat: Int
-        get() {
-            return when (spiciness) {
-                "mild" -> 1
-                "medium" -> 3
-                "spicy" -> 5
-                "very spicy" -> 7
-                "extremely spicy" -> 10
-                else -> 0
-            }
-        }
+fun main() {for(element in spiceCabinet) println(element.label)}
+
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF),YELLOW(0xFFFF00);
 }
 
-val spices1 = listOf(
-        Spice("curry", "mild"),
-        Spice("pepper", "medium"),
-        Spice("cayenne", "spicy"),
-        Spice("ginger", "mild"),
-        Spice("red curry", "medium"),
-        Spice("green curry", "mild"),
-        Spice("hot pepper", "extremely spicy")
-)
+abstract sealed class Spice(val name: String, val spiciness: String = "mild", color: SpiceColor) : SpiceColor by color {
+    abstract fun prepareSpice()
+}
 
-val spice = Spice("cayenne", spiciness = "spicy")
+class Curry(name: String, spiciness: String,
+            color: SpiceColor = YellowSpiceColor) : Spice(name, spiciness, color), Grinder {
+    override fun grind() {
+    }
 
-val spicelist = spices1.filter {it.heat < 5}
+    override fun prepareSpice() {
+        grind()
+    }
+}
 
-fun makeSalt() = Spice("Salt")
+interface Grinder {
+    fun grind()
+}
+
+interface SpiceColor {
+    val color: Color
+}
+
+object YellowSpiceColor : SpiceColor {
+    override val color = Color.YELLOW
+}
+
+
+data class SpiceContainer(var spice: Spice) {
+    val label = spice.name
+}
+
+val spiceCabinet = listOf(SpiceContainer(Curry("Yellow Curry", "mild")),
+        SpiceContainer(Curry("Red Curry", "medium")),
+        SpiceContainer(Curry("Green Curry", "spicy")))
